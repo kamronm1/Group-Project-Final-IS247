a51e218aa6737dd04a427d80c2c879fdf31933f6
Dbelloff <dbelloff23@gmail.com>
2026-05-10 22:31:50 -0400

    add comments for java docs

--- src/OutOfStockException.java
+++ src/OutOfStockException.java
@@ -1,5 +1,6 @@
 /**
  * Exception that is thrown when item is out of stock
+ * Extends exception
  */
 public class OutOfStockException extends Exception {
     public OutOfStockException(String message) {
